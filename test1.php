e2048618531a534096f9e9704ad1c553c67d2df3
greg skiano work <gregoryskiano@gmail.com>
2013-02-08 12:57:11 -0500

    adding test page

---
+++ test1.php
@@ -0,0 +1,73 @@
+<?php 
+	
+	require_once('php/grid-fill.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grid Test</title>
+
+	 <meta charset='utf-8'> 
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/collage.css">
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+
+</head>
+<body>
+	<!--
+	<div class="test_wrap">
+
+		<h1> Grid Test </h1>
+
+
+		<div class="web_type_collage">
+
+			<div class="zone x0 y4 w3 h2">
+				<div class="trim">
+				</div>
+				<div class="bleed">
+				</div>
+			</div>
+
+			<div class="zone x3 y0 w1 h4">
+				<div class="trim">
+				</div>
+				<div class="bleed">
+				</div>
+			</div>
+
+			<div class="zone x0 y0 w3 h1">
+				<div class="trim">
+				</div>
+				<div class="bleed">
+				</div>
+			</div>
+
+			<div class="zone x2 y1 w1 h3">
+				<div class="trim">
+				</div>
+				<div class="bleed">
+				</div>
+			</div>
+
+			<div class="zone x4 y0 w1 h6">
+				<div class="trim">
+				</div>
+				<div class="bleed">
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+
+	-->
+
+</body>
+</html>
