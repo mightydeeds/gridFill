58ad48866377104289bb7a3229f0b364b2026f5a
Greg Skiano <gregoryskiano@gmail.com>
2013-02-28 23:53:53 -0500

    more performance stuff

--- grid2.php
+++ grid2.php
@@ -6,7 +6,7 @@
 
 	class rectGrid {
 
-		public $supply_grid = array();
+		public $supply_grid = "";
 		public $final_grid  = array();
 		public $g_width     = 0;
 		public $g_height    = 0;
@@ -20,42 +20,49 @@
 			$this->g_height = $g_height;
 			$this->target   = $target;
 
+			echo "expected grid: ".$this->testSize($this->g_width,$this->g_height)."\n\n";
+
 			$start = microtime(true); // **********************************
 			
+
+			$before = memory_get_usage();
 			$this->fillSupply($this->g_width,$this->g_height);
+			$after = memory_get_usage();
+
+			echo "memoory from grid fill: ".($after-$before)."\n";
 			
 			$stop = microtime(true); // **********************************
 			printf("\ngrid:\t%.5f seconds\n", $stop-$start); // ******
 			
-			$start = microtime(true); // **********************************
-			shuffle($this->supply_grid);
-			$stop = microtime(true); // **********************************
-			printf("\nshuff:\t%.5f seconds\n", $stop-$start); // ******
+			// $start = microtime(true); // **********************************
+			// shuffle($this->supply_grid);
+			// $stop = microtime(true); // **********************************
+			// printf("\nshuff:\t%.5f seconds\n", $stop-$start); // ******
 
-			if($this->target){
-				self::$target_area = $this->g_width * $this->g_height / $this->target;
+			// if($this->target){
+			// 	self::$target_area = $this->g_width * $this->g_height / $this->target;
 
-				$start = microtime(true); // **********************************
-				usort($this->supply_grid, array('rectGrid','sortShapes'));
-				$stop = microtime(true); // **********************************
-				printf("sort:\t%.5f seconds\n", $stop-$start); // ******
-			}
+			// 	$start = microtime(true); // **********************************
+			// 	usort($this->supply_grid, array('rectGrid','sortShapes'));
+			// 	$stop = microtime(true); // **********************************
+			// 	printf("sort:\t%.5f seconds\n", $stop-$start); // ******
+			// }
 
-			$total_choosing = 0;
+			// $total_choosing = 0;
 
-			while (count($this->supply_grid)>0) {
+			// while (count($this->supply_grid)>0) {
 				
-				$start = microtime(true); // **********************************
-				$this->chooseShape();
-				$stop = microtime(true); // **********************************
-				$total_choosing += $stop-$start;
-				printf("shape:\t%.5f \n", $stop - $start); // ******
+			// 	$start = microtime(true); // **********************************
+			// 	$this->chooseShape();
+			// 	$stop = microtime(true); // **********************************
+			// 	$total_choosing += $stop-$start;
+			// 	printf("shape:\t%.5f \n", $stop - $start); // ******
 
-			}
+			// }
 
-			printf("shapes:\t%.5f \n", $total_choosing); // ******
+			// printf("shapes:\t%.5f \n", $total_choosing); // ******
 
-			// echo $this->checkGrid($this->final_grid,40,30);
+			// // echo $this->checkGrid($this->final_grid,40,30);
 
 		}
 
@@ -101,21 +108,63 @@
 
 		public function fillSupply($w,$h) {
 
-			$shapes = array();
+			// $y_edge  = range(1,$h);
+			// $x_edge  = range(1,$w);
+			// reset($y_edge);
+			// reset($x_edge);
 
-			for($yi=0; $yi < $h; $yi++){
-				for($xi=0; $xi < $w; $xi++) {
-					// for every starting point
-					for($xf=$xi; $xf < $w; $xf++) {
-						for( $yf=$yi; $yf < $h; $yf++) {
-							$shapes[] = $xi.".".$yi.".".$xf.".".$yf;
+			// print_r($x_edge);
+			
+			// while($next = next($y_edge)){
+
+			// 	$y1 = key($y_edge);
+			// 	// $y_point = range($y1,$h);
+			// 	// reset($y_point);
+
+			// 	echo $y1;
+
+			// 	while($next = next($x_edge)){
+
+			// 		$x1 = key($x_edge)  - 1;
+			// 		$x_point = range($x1,$w);
+			// 		reset($x_point);
+			// 		// echo $x1.".".$y1."\n";
+
+			// 		// for every starting point
+			// 		// while($next = next($x_point)){
+			// 		// 	while($next = next($y_point)){
+							
+			// 		// 		$x2 = key($x_point) - 1;
+			// 		// 		$y2 = key($y_point) - 1;
+			// 		// 		// echo "Box: ".$x1.".".$y1." | ".$x2.".".$y2."\n";
+			// 		// 		// $this->supply_grid[] = $x1.".".$y1.".".$x2.".".$y2;
+
+			// 		// 	}
+			// 		// 	reset($y_point);	
+			// 		// }
+			// 		// reset($x_point);
+			// 	}
+
+			// }
+			for ($y1 = 0; $y1 < $h; $y1++) {
+				for ($x1=0; $x1 < $w; $x1++) {
+					
+					for($x2 = $x1; $x2 < $w; $x2 ++) {
+						
+						for($y2 = $y1; $y2 < $h; $y2++) {
+							// echo "Box: ".$x1.".".$y1." | ".$x2.".".$y2."\n";
+							$this->supply_grid .= $x1.".".$y1.".".$x2.".".$y2."|";
+							$shape = $x1.".".$y1.".".$x2.".".$y2;
 						}
-
+										
 					}
 				}
 			}
 
-			$this->supply_grid = $shapes;
+			// for($i=0;$i<$this->testSize($this->g_width,$this->g_height);$i++){
+			// 	$this->supply_grid .= "ssdfsdfsdfsdfsdfsdfsdfsdfsdf342344324|";
+			// }
+
 
 		}
 
@@ -174,7 +223,7 @@
 		// $test1 = new rectGrid(10,10,3);
 		// $test2 = new rectGrid(10,10,5);
 		// $test3 = new rectGrid(10,10,10);
-		$test4 = new rectGrid(15,15,15);
+		$test4 = new rectGrid(50,40);
 		// $test5 = new rectGrid(10,10,50);
 	$stop = microtime(true);
 	printf("\nall grids: %.5f seconds\n", $stop-$start);
